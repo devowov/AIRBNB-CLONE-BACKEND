50039491ace1e052e8d1fa0892ca81fc7b6f0f09
EUNJU HWANG <dev.owov@gmail.com>
2024-10-02 02:03:23 +0900

    Assignment 26 submit

--- tweets/admin.py
+++ tweets/admin.py
@@ -2,14 +2,36 @@ from django.contrib import admin
 from .models import Tweet, Like
 
 # Register your models here.
+class ElonMuskFilter(admin.SimpleListFilter):
+    title = 'Elon Musk'
+    parameter_name = 'elon_musk'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('contains', 'Contains "Elon Musk"'),
+            ('not_contain', 'Does not contain "Elon Musk"'),
+        )
+
+    def queryset(self, request, tweets):
+        if self.value() == 'contain':
+            return tweets.filter(payload__icontains='Elon Musk')
+        elif self.value() == 'not_contain':
+            return tweets.exclude(payload__icontains='Elon Musk')
+        return tweets
+
 @admin.register(Tweet)
 class TweetAdmin(admin.ModelAdmin):
     list_display = ('user'
                     , 'payload'
                     , 'created_at')
+    
+    search_fields = ['payload', 'user__username']
+    list_filter = ['created_at', ElonMuskFilter]
 
 @admin.register(Like)
 class LikeAdmin(admin.ModelAdmin):
     list_display = ('user'
                     , 'tweet'
                     , 'created_at')
+    search_fields = ['user__username']
+    list_filter = ['created_at']
